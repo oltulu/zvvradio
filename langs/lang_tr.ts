a4b5a1a07252d3a81ae492550685a5f7e4f4b3d8
Aylinux <cihanalk@gmail.com>
2020-06-04 14:38:17 +0300

    Create lang_tr.ts

---
+++ langs/lang_tr.ts
@@ -0,0 +1,309 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>Tab2_stations</name>
+    <message>
+        <source>Edit</source>
+        <translation>Düzenle</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Bilgi</translation>
+    </message>
+    <message>
+        <source>Remove fully</source>
+        <translation>Tümünü sil</translation>
+    </message>
+    <message>
+        <source>Search + press Enter</source>
+        <translation>Ara + Enter&apos;e bas</translation>
+    </message>
+    <message>
+        <source>Add to Main</source>
+        <translation>Ana Bölüme Ekle</translation>
+    </message>
+    <message>
+        <source>Genres:</source>
+        <translation>Tür:</translation>
+    </message>
+</context>
+<context>
+    <name>Info_Win</name>
+    <message>
+        <source>Info</source>
+        <translation>Bilgi</translation>
+    </message>
+    <message>
+        <source>Url:</source>
+        <translation>Url:</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>İsim:</translation>
+    </message>
+</context>
+<context>
+    <name>Tab1_main</name>
+    <message>
+        <source>Info</source>
+        <translation>Bilgi</translation>
+    </message>
+    <message>
+        <source>Remove from Main</source>
+        <translation>Ana Bölümden Sil</translation>
+    </message>
+</context>
+<context>
+    <name>Add_Station</name>
+    <message>
+        <source>Save</source>
+        <translation>Kaydet</translation>
+    </message>
+    <message>
+        <source>URL:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>ICON:</source>
+        <translation>SİMGE:</translation>
+    </message>
+    <message>
+        <source>NAME:</source>
+        <translation>İSİM:</translation>
+    </message>
+    <message>
+        <source>Enter name of station --&gt;	</source>
+        <translation>Radyo adı gir --&gt;	</translation>
+    </message>
+    <message>
+        <source>Choose genre of station --&gt; </source>
+        <translation>Radyo türü seç --&gt; </translation>
+    </message>
+    <message>
+        <source>COUNTRY:</source>
+        <translation>ÜLKE:</translation>
+    </message>
+    <message>
+        <source>GENRE:</source>
+        <translation>TÜR:</translation>
+    </message>
+    <message>
+        <source>Add station to Base</source>
+        <translation>Baz istasyonuna Radyo ekle</translation>
+    </message>
+    <message>
+        <source> was added.</source>
+        <translation> eklendi.</translation>
+    </message>
+    <message>
+        <source>Choose image for station --&gt; </source>
+        <translation>Radyo için simge seçin--&gt; </translation>
+    </message>
+    <message>
+        <source>Choose icon</source>
+        <translation>Simge seç</translation>
+    </message>
+    <message>
+        <source>Choose:</source>
+        <translation>Seçin:</translation>
+    </message>
+    <message>
+        <source>Add station</source>
+        <translation>Radyo Ekle</translation>
+    </message>
+    <message>
+        <source>Choose country of station --&gt; </source>
+        <translation>Radyo ülkesi seç --&gt; </translation>
+    </message>
+    <message>
+        <source>Add new station:</source>
+        <translation>Yeni radyo ekle:</translation>
+    </message>
+    <message>
+        <source>The station </source>
+        <translation>Radyo </translation>
+    </message>
+    <message>
+        <source>Choose: </source>
+        <translation>Seçin: </translation>
+    </message>
+    <message>
+        <source>Put your stream --&gt;	</source>
+        <translation>Akışınızı koyun--&gt;	</translation>
+    </message>
+</context>
+<context>
+    <name>Tab4_config</name>
+    <message>
+        <source>Change language:</source>
+        <translation>Dili Değiştir:</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Kapat</translation>
+    </message>
+    <message>
+        <source>PANEL</source>
+        <translation>PANEL</translation>
+    </message>
+    <message>
+        <source>Panel</source>
+        <translation>Panel</translation>
+    </message>
+    <message>
+        <source>Time:</source>
+        <translation>Süre:</translation>
+    </message>
+    <message>
+        <source>Minimize to panel:</source>
+        <translation>Paneli küçült:</translation>
+    </message>
+    <message>
+        <source>After:</source>
+        <translation>Sonra:</translation>
+    </message>
+    <message>
+        <source>Put your stream here and press &apos;Check&apos;</source>
+        <translation>Akışınızı buraya girin ve &apos;Kontrol Et&apos;e basın</translation>
+    </message>
+    <message>
+        <source>GLOBAL</source>
+        <translation>GENEL</translation>
+    </message>
+    <message>
+        <source>RECORD</source>
+        <translation>KAYIT</translation>
+    </message>
+    <message>
+        <source>Stream</source>
+        <translation>Akış</translation>
+    </message>
+    <message>
+        <source>Choose:</source>
+        <translation>Seçin:</translation>
+    </message>
+    <message>
+        <source>Set timer: </source>
+        <translation>Zamanlayıcı Ayarla: </translation>
+    </message>
+    <message>
+        <source>Suspend</source>
+        <translation>Askıya Al</translation>
+    </message>
+    <message>
+        <source>Open records folder:</source>
+        <translation>Kayıt Klasörünü Aç:</translation>
+    </message>
+    <message>
+        <source>Add station:</source>
+        <translation>Radyo Ekle:</translation>
+    </message>
+    <message>
+        <source>Switch to Panel mode:</source>
+        <translation>Panel Modunu Değiştir:</translation>
+    </message>
+    <message>
+        <source>Shutdown</source>
+        <translation>Kapat</translation>
+    </message>
+</context>
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Hakkında</translation>
+    </message>
+    <message>
+        <source>Follow to my blog:&lt;a href = &apos;http://zvvubuntu.blogspot.com&apos;&gt;ZVVUbuntu`s Blog&lt;/a&gt;</source>
+        <translation>Blogumu takip et:&lt;a href = &apos;http://zvvubuntu.blogspot.com&apos;&gt;ZVVUbuntu&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>Author: &lt;b&gt;Vyacheslav Zubik&lt;/b&gt;. Ukraine, Kherson</source>
+        <translation>Yazar: &lt;b&gt;Vyacheslav Zubik&lt;/b&gt;. Ukrayna, Kherson</translation>
+    </message>
+    <message>
+        <source>Have a nice day!</source>
+        <translation>Mutlu günler!</translation>
+    </message>
+</context>
+<context>
+    <name>Widgets</name>
+    <message>
+        <source>Panel</source>
+        <translation>Panel</translation>
+    </message>
+    <message>
+        <source>Add station</source>
+        <translation>Radyo Ekle</translation>
+    </message>
+</context>
+<context>
+    <name>Hpanel</name>
+    <message>
+        <source>Show stations</source>
+        <translation>Radyoları Göster</translation>
+    </message>
+    <message>
+        <source>Show info</source>
+        <translation>Bilgi göster</translation>
+    </message>
+    <message>
+        <source>Close app</source>
+        <translation>Uygulamayı kapat</translation>
+    </message>
+    <message>
+        <source>Normal mode</source>
+        <translation>Normal mod</translation>
+    </message>
+</context>
+<context>
+    <name>Edit</name>
+    <message>
+        <source>Change icon:</source>
+        <translation>Simge değiştir:</translation>
+    </message>
+    <message>
+        <source>Change stream url:</source>
+        <translation>Akış urlsini değiştir:</translation>
+    </message>
+    <message>
+        <source>Unlock</source>
+        <translation>Kilidi kaldır</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Güncelle</translation>
+    </message>
+    <message>
+        <source>Choose icon</source>
+        <translation>Simge seç</translation>
+    </message>
+    <message>
+        <source>Edit station</source>
+        <translation>Radyo düzenle</translation>
+    </message>
+</context>
+<context>
+    <name>Search_radio</name>
+    <message>
+        <source>Search</source>
+        <translation>Ara</translation>
+    </message>
+</context>
+<context>
+    <name>Pls</name>
+    <message>
+        <source>Stop load</source>
+        <translation>Yüklemeyi durdur</translation>
+    </message>
+    <message>
+        <source>Open File</source>
+        <translation>Dosya aç</translation>
+    </message>
+    <message>
+        <source>Files (*.pls *.m3u *.m3u8)</source>
+        <translation>Dosya (*.pls *.m3u *.m3u8)</translation>
+    </message>
+</context>
+</TS>
